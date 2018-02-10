2800b084e0c0dc927480feb8d769243f3703c7e9
Patrick_Wang_Diyai <wxmgcs@sina.cn>
2018-02-10 11:49:02 +0800

    源码分析笔记

--- src/main/java/com/ctu/zookeeper/ZKConnector.java
+++ src/main/java/com/ctu/zookeeper/ZKConnector.java
@@ -27,6 +27,12 @@ public class ZKConnector {
 
 
     public ZooKeeper connect(String host, Watcher watcher)throws IOException,InterruptedException,KeeperException {
+        //1)客户端发起命令请求
+        //2)构造Packet，调用ClientCnxn的submitRequest方法。ClientCnxn的submitRequest方法调用queuePacket方法将Request放入outgoingQueue队列中，然后调用线程wait方法等待该Request从服务端获得响应为止。
+        //3)ClientCnxnSocketNetty和ClientCnzx共享同一个outgoingQueue，ClientCnxnSocketNetty启动了发送守护进程，当outgoingQueue队列中有Request时，会自动将该Request发送给ZooKeeperServer。同时ClientCnxnSocketNetty启动接收线程实时接收ZooKeeperServer的返回数据，返回数据触发ClientCnxnSocketNetty中启动的ZKClientHandler的MessageReceived事件。
+        //4)在MessageReceived事件中调用ClientCnxn中的SendThread的readResponse方法。
+        //5)readResponse方法中最后调用finishPacket方法唤醒在该请求上wait的2）中的submitRequest方法。使得submitRequest方法返回到客户端。
+       /// 6)客户端获得返回的命令执行结果。
         zk = new ZooKeeper(host, Constant.SESSION_TIMEOUT, watcher);
         return zk;
 
@@ -39,11 +45,13 @@ public class ZKConnector {
     }
 
     public void update(String path,byte[] data) throws InterruptedException,KeeperException{
+        //设置节点关联数据
         zk.setData(path,data,zk.exists(path,true).getVersion());
 
     }
 
     public byte[] getData(String path)throws InterruptedException,KeeperException{
+        //获取节点关联数据
         return zk.getData(path,true,zk.exists(path,true));
     }
 
@@ -54,6 +62,9 @@ public class ZKConnector {
 
     public boolean create(String path,byte[] data)throws InterruptedException,KeeperException{
         try {
+            //create方法根据输入参数构造出CreateRequest包，然后通过submitRequest方法传递给服务端，
+            // submitRequest方法将CreateRequest转换成Packet包并调用sendPacket方法将发送包放入队列，等待发送线程发送给服务端。
+            //服务端响应完成后会将返回结果填充到CreateResponse实体中返回给客户端。
             zk.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             return true;
         }catch (Exception ex){
@@ -92,6 +103,7 @@ public class ZKConnector {
     }
 
     public List<String> getChilds(String path)throws InterruptedException,KeeperException{
+        //获取子节点路径列表
         return zk.getChildren(path,true);
     }
 
@@ -101,6 +113,17 @@ public class ZKConnector {
 
     public void close() throws InterruptedException{
         zk.close();
-
     }
+
+
+    public void isExists(String path) throws InterruptedException,KeeperException{
+        //判断节点是否存在，异步方式，并且在回调函数中触发事件观察者
+        zk.exists(path,true);
+    }
+
+//    public void startWebServer() throws  ClassNotFoundException{
+//        Class<?> jettyAdminServerC = Class.forName("org.apache.zookeeper.server.admin.JettyAdminServer");
+//        object adminServer = jettyAdminServerC.getConstructor(null).newInstance();
+//    }
+
 }
--- src/test/java/TestZKConnector.java
+++ src/test/java/TestZKConnector.java
@@ -1,7 +1,8 @@
 import com.ctu.zookeeper.Constant;
 import com.ctu.zookeeper.ZKConnector;
 import junit.framework.TestCase;
-import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.client.HostProvider;
 import org.apache.zookeeper.data.ACL;
 
 import java.io.IOException;
@@ -45,7 +46,32 @@ public class TestZKConnector extends TestCase {
             System.out.println(acl.toString());
         }
 
+        zkc.isExists(path);
 
+        zkc.getChilds(path);
+
+    }
+
+    public void testClientCnxn() throws IOException{
+
+        //Clientcnxn将客户端请求加入发送队列，等待sendThread发送。eventThread负责处理Server返回的WatchedEvent，回调注册的客户端事件接口处理函数。
+        
+        //sendPacket => 客户端调用SendThread的sendPacket方法发送数据
+        
+//        finishPacket => 接收到服务端的响应，唤醒等待响应的客户端线程。
+// 如果客户端请求指定了Watcher，则同时生成WatchedEvent事件并放入事件队列，等待eventThread线程处理。
+// 客户端请求在写操作的情况下，发送完命令给服务端后会阻塞等待服务端响应，当ClientCntx收到服务端响应后调用finishPacket方法，唤醒阻塞在该Packet的客户端，客户端至此收到响应。
+//        String chrootPath = "";
+//        HostProvider hostProvider =null;  可以连接的ZK服务器列表
+//        int sessionTimeout = 300000;
+//        ZooKeeper zooKeeper =null;
+//        ClientWatchManager watcher = null;
+//        ClientCnxnSocket clientCnxnSocket =null;
+//        long sessionId = 0;
+//        byte[] sessionPasswd = "123456".getBytes();
+//        boolean canBeReadOnly = false;
+//        ClientCnxn clientCnxn = new ClientCnxn(chrootPath, hostProvider, sessionTimeout, zooKeeper,
+//                watcher, clientCnxnSocket, canBeReadOnly);
 
     }
 
